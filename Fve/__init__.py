a7fc02de6e262c94ca59227f7368dbcfa3330832
JirkaR747 <jiri.rauser@poly-analyser.cz>
2026-01-12 16:22:46 +0100

    Delete Fve directory

--- Fve/__init__.py
+++
@@ -1,9 +0,0 @@
-# # __init__.py
-# # Spouštěcí soubor pro celou aplikaci
-#
-# # Import hlavní funkce menu
-# from menu import main
-#
-# # Spuštění CLI menu při importu balíčku nebo přímém spuštění
-# if __name__ == "__main__":
-#     main()
